069859f29b553703502383cacde90611686cdb1b
smvamsi <smvamsi@smvamsi.local>
2020-05-14 21:12:30 +0530

    new-resolutions

--- dots-setup.sh
+++ dots-setup.sh
@@ -5,21 +5,10 @@ if [ -d "${HOME}/Code" ];then
 	echo "A ${HOME}/Code directory already exists!\nGoing Full Speed Ahead!\n"
 else
 	mkdir ${HOME}/Code
-	cd ${HOME}/Code
 fi
 
-./repo-clone.sh
-
-if [ $? -eq 1 ];then
-	echo "Something wrong with the ${HOME}/Code directory"
-	break;
-fi
-
-# is it linux?
-# linux=0	# not linux by default
-
-# read linux variable.
-# read -p "Enter 1 if it's linux or Just press enter if it's mac: " linux
+# clone some repos you're working on...
+/repo-clone.sh
 
 if [ $(uname) = Darwin ]; then			# using uname is better than reading user's input
 	echo "Treating the System as Mac os\n"
@@ -28,12 +17,14 @@ else
 	cd "${HOME}/Code/dots/"
 fi
 
-cd $HOME && for file in ${HOME}/Code/dots/.*; do ln -sv ${file}; done
+for file in ${HOME}/Code/dots/.*; do ln -sv ${file}; done
 
 echo "Removing .gitconfig and .gitignore"
 rm ${HOME}/.gitconfig ${HOME}/.gitignore 
 echo "Removing .git/"
 rm -rf ${HOME}/.git
+
+echo "source .source_list" >> ${HOME}/.zshrc
 echo "Sourcing .zshrc"
 source ${HOME}/.zshrc
 
--- repo-clone.sh
+++ repo-clone.sh
@@ -12,22 +12,11 @@ set -o pipefail
 # An array of your repositories
 repos=(
 	"smvamsi96.github.io"
-	"lexie"
 	"knives"
 )
-
-if [ -d "$HOME/Code" ];then
-	cd "$HOME/Code"
-	echo "A Code directory Already exists"
-	echo "Going Full Speed Ahead!"
-else
-	echo "Creating a Code directory in $HOME"
-	mkdir "$HOME/Code"
-	cd "$HOME/Code"
-fi
 
 # Internal Field Separator(ask google if you don't know what it is!)
 IFS=$'\n\t'
 
 # Clone all the repos in one sweep!
-for repo in ${repos[@]}; do git clone https://github.com/smvamsi96/${repo}.git && echo "\n"; done
+for repo in ${repos[@]}; do cd ${HOME}/Code;git clone https://github.com/smvamsi96/${repo}.git && echo "\n"; done
